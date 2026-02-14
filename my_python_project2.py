4a0d012bf914889da645d93d14dedee459c14e31
surajsaarsar69-bit <surajsaarsar69@gmail.com>
2026-02-14 11:06:19 +0530

    Add files via upload

---
+++ my_python_project2.py
@@ -0,0 +1,11 @@
+#taking input from the user
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+
+#concatenating first name and last name with a space in between
+full_name=first_name+" "+last_name
+
+#printing the final greeting message
+print("hello,"+ full_name +"!welcome to the python program.")
+
+
